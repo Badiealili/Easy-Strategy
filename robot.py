4b5dcfa053893145584c7f5f14e61cfeda321c2e
badiealili <alili31badie@gmail.com>
2025-02-11 18:52:29 +0100

    Draw robot arms as a separate rectangle

--- robot.py
+++ robot.py
@@ -14,9 +14,8 @@ class Robot:
         self.width = width
         self.height = height
         self.canvas = canvas
-        self.id = None
+        self.shapes = []
         self.update(self.x, self.y, self.angle)
-        # self.id = self.canvas._canvas.create_rectangle(self.x - self.width // 2, self.y - self.height // 2, self.x + self.width // 2, self.y + self.height // 2, fill="green")
 
     def update(self, x: int, y: int, angle: int) -> None:
         """Update the robot position and angle with the given parameters.
@@ -25,14 +24,15 @@ class Robot:
         self.x = x
         self.y = y
         self.angle = angle
-        self.canvas._canvas.delete(self.id)
+        for shape in self.shapes:
+            self.canvas._canvas.delete(shape)
         # TODO: Recreate the shape to accurately represent the robot and represent its direction
-        # x coordinates of the polygon
+        # x coordinates of the robot polygon
         x1 = self.x - self.width // 2
         x2 = self.x + self.width // 2
         x3 = x2
         x4 = x1
-        # y coordinates of the polygon
+        # y coordinates of the robot polygon
         y1 = self.y - self.height // 2
         y2 = y1
         y3 = self.y + self.height // 2
@@ -41,7 +41,24 @@ class Robot:
         # Rotated points
         rotated_points = self.rotate(angle=angle, coords=[x1, y1, x2, y2, x3, y3, x4, y4])
         [(x1, y1), (x2, y2), (x3, y3), (x4, y4)] = rotated_points
-        self.id = self.canvas._canvas.create_polygon(x1, y1, x2, y2, x3, y3, x4, y4, fill="green")
+        self.shapes.append(self.canvas._canvas.create_polygon(x1, y1, x2, y2, x3, y3, x4, y4, fill="green"))
+        # Repeat the same operation but for the robot's arm:
+        # x coordinates of the robot arm polygon
+        x1 = self.x + 0.25*self.width
+        x2 = self.x + self.width // 2
+        x3 = x2
+        x4 = x1
+        
+        # y coordinates of the robot arm polygon
+        y1 = self.y - self.height // 2
+        y2 = y1
+        y3 = self.y + self.height // 2
+        y4 = y3
+
+        # Rotated points
+        rotated_points = self.rotate(angle=angle, coords=[x1, y1, x2, y2, x3, y3, x4, y4])
+        [(x1, y1), (x2, y2), (x3, y3), (x4, y4)] = rotated_points
+        self.shapes.append(self.canvas._canvas.create_polygon(x1, y1, x2, y2, x3, y3, x4, y4, fill="blue"))
 
     def reset(self) -> None:
         """Reset the robot to its initial parameters."""
@@ -58,8 +75,8 @@ class Robot:
 
         return xr + cx, yr + cy
 
-    def rotate(self, angle: int, coords) -> list[tuple[int, int]]:
-        """Rotate the robot rectangle with *angle* degrees around its center"""
+    def rotate(self, angle: int, coords: tuple[int, int]) -> list[tuple[int, int]]:
+        """Rotate the robot rectangle with *angle* degrees around the robot's center point"""
         [x1, y1, x2, y2, x3, y3, x4, y4] = coords
         points = [(x1, y1), (x2, y2), (x3, y3), (x4, y4)]
         new_points = [self.rotate_point(x=x, y=y, angle=angle, cx=self.x, cy=self.y) for x,y in points]
